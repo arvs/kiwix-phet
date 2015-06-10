5127a50081daa8d2cfc910550a3f0ffdc99b51de
Arvind Srinivasan <arvnd@arvnd-macbookpro.roam.corp.google.com>
2015-06-10 17:10:19 +0200

    add cl args

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ from os import path
 import requests
 import codecs
 
-from sh import git, mkdir, curl, cd, npm
+from argparse import ArgumentParser
+from sh import git, mkdir, curl, cd, npm, cp
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 
@@ -35,18 +36,19 @@ class PhetScraper(object):
           f.write(r.content)
     json.dump(self.repos, open("repositories.json", "wb"))
 
-  def generate_index(self):
+  def generate_index(self, locales="en"):
     print "Generating Index"
     base_path = path.dirname(path.realpath(__file__))
     env = Environment(loader=FileSystemLoader(base_path))
     template = env.get_template('index.html')
-    sims = {name : u"%s_en.html" % name for name in self.repos}
-    with codecs.open("%s/index.html" % path.join(base_path, "sims"), 'w', 'utf-8') as f:
-      f.write(template.render(sims = sims).decode('utf-8', 'ignore'))
+    for l in locales.split(','):
+      sims = {name : u"%s_%s.html" % (name, l) for name in self.repos}
+      with codecs.open("%s/index_%s.html" % (path.join(base_path, "sims"), l), 'w', 'utf-8') as f:
+        f.write(template.render(sims = sims).decode('utf-8', 'ignore'))
 
   def update_repos(self):
     git.submodule.update()
-    for name, repo in self.repos:
+    for name, repo in self.repos.iteritems():
       git.clone(repo)
 
   def compile_simulations(self, locales="en"):
@@ -60,18 +62,26 @@ class PhetScraper(object):
     # cd ..
     # rm -rf $name
     for name in self.repos:
-      cd(name)
-      # npm.install()
-      # grunt.build(force=True, locales=locales)
-      cp("*.html", "../sims")
-      cp("images/*", "../sims/images")
-      cp("js/*", "../sims/js")
-      cd("..")
-      rm("-rf", name)
+      if path.exists(name):
+        cd(name)
+        # npm.install()
+        # grunt.build(force=True, locales=locales)
+        cp("*.html", "../sims")
+        cp("images/*", "../sims/images")
+        cp("js/*", "../sims/js")
+        cd("..")
+        rm("-rf", name)
 
 if __name__ == '__main__':
+  parser = ArgumentParser()
+  parser.add_argument("--scrape_thumbs", help="scrape thumbnails", action="store_true")
+  parser.add_argument("--download_repos", help="clone the git repos", action="store_true")
+  parser.add_argument("--locales", default = "en", help="short form locales, i.e. ar,fr,es. English by default.")
+  args = parser.parse_args()
   scraper = PhetScraper()
-  scraper.scrape_thumbnails()
-  scraper.generate_index()
-  scraper.update_repos()
-  scraper.compile_simulations()
+  if args.scrape_thumbs:
+    scraper.scrape_thumbnails()
+  scraper.generate_index(args.locales)
+  if args.download_repos:
+    scraper.update_repos()
+  scraper.compile_simulations(args.locales)
